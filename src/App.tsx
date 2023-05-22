d9b207b9327c7ff9a728ae95f7fc719c132d558a
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-05-22 13:31:13 +0700

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-red-400'>ágdjhas</p>     
+    </>
+  )
+}
+
+export default App
