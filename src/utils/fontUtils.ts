f92528cc27bbd22b2d8b8ac84f8608113a276d27
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-05-23 15:46:13 +0700

    display font style

--- src/utils/fontUtils.ts
+++ src/utils/fontUtils.ts
@@ -1,6 +1,8 @@
-const addFont = (name: string, url: string) => {
+const addFont = (name: string, url= "") => {
     const newStyle = document.createElement('style');
-    newStyle.appendChild(document.createTextNode('@font-face{font-family: '+name+'; src: url('+url+');}'));
+    let formatedUrl = url
+    if(!formatedUrl.includes("https")) formatedUrl = formatedUrl.replace("http", "https")
+    newStyle.appendChild(document.createTextNode('@font-face{font-family: '+name+'; src: url('+formatedUrl+');}'));
     document.head.appendChild(newStyle)
 
     return newStyle
