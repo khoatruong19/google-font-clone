e829b5f37f11f03f5bc984afca02b7ec7d6ee453
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-05-23 10:44:13 +0700

    format code

--- src/utils/fontUtils.ts
+++ src/utils/fontUtils.ts
@@ -1,7 +1,7 @@
 const addFont = (name: string, url: string) => {
     const newStyle = document.createElement('style');
     newStyle.appendChild(document.createTextNode('@font-face{font-family: '+name+'; src: url('+url+');}'));
-    document.body.appendChild(newStyle)
+    document.head.appendChild(newStyle)
 }
 
 export {addFont}
