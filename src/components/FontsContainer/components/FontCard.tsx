e239121baafd2a79e6902c0d3a4fbef7397c6125
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-05-23 10:55:00 +0700

    fix load font production error

--- src/components/FontsContainer/components/FontCard.tsx
+++ src/components/FontsContainer/components/FontCard.tsx
@@ -1,10 +1,31 @@
 import { IFontInGeneral } from '../../../stores/fontStore';
 import { DEFAULT_SAMPLE_SENTENCES } from '../constants';
+import {useState,useEffect} from "react"
 interface IProps {
   font: IFontInGeneral;
 }
 
 const FontCard = ({ font }: IProps) => {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+
+  const handleLoadFont = () => {
+    if (fontLoaded) {
+      return
+    }
+    const checkFont = async () => {
+      const fontObserver = new FontFaceObserver(font.family)
+      try {
+        await fontObserver.load(null, 7000)
+        setFontLoaded(true)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    checkFont()
+  }
+
+  useEffect(handleLoadFont, [font, fontLoaded])
   return (
     <div className="h-[100%] min-h-[300px] w-[100%] border-[1.5px] rounded-lg">
       <div className="py-3 px-4 pb-5">
